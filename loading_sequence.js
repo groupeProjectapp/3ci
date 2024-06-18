e0767f18645cfe0e009578743f251f72720a0484
Yahia <aitouryahia@gmail.com>
2024-06-18 12:52:47 +0100

    Base

---
+++ loading_sequence.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    let isLoad = false
+    if (isLoad) {
+        setTimeout(function () {
+            $(".its_background").fadeOut(500, function () {
+                $(".its_background").removeClass("pre")
+                $(".its_background").fadeIn(1000, function () {
+                    setTimeout(function () {
+                        $(".background").fadeOut(800, function () {
+                            $(".site").addClass("show");
+                        });
+                    }, 100)
+                })
+            })
+
+        }, 1000)
+    }else {
+        $(".site").addClass("show");
+    }
+
+})
